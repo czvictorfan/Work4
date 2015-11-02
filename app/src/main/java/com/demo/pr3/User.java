21e19ceb402b450cd693129767ad2621f236b640
QinhongFan <534225388@qq.com>
2015-11-02 21:56:59 +0800

    message

--- app/src/main/java/com/demo/pr3/User.java
+++ app/src/main/java/com/demo/pr3/User.java
@@ -1,72 +1,64 @@
 package com.demo.pr3;
 
-/**
- * Created by Administrator on 2015/10/28.
- */
+
 public class User {
-
-    private String name;//用户名
-    public final static String NAME ="name";
-    private String mobile;//手机号码
-    public final static String MOBILE ="mobile";
-    private String danwei;//单位
-    public final static String DANWEI ="danwei";
-    private String qq;//QQ
-    public final static String QQ ="qq";
-    private String address;//地址
-    public final static String ADDRESS ="id_DB";
-
-    private int id_DB=-1;//表主键ID
+    public final static String NAME="name";
+    public final static String MOBILE="mobile";
+    public final static String DANWEI="danwei";
+    public static final String QQ="qq";
+    public static final String ADDRESS="address";
+    private String name;
+    private String mobile;
+    private String danwei;
+    private String qq;
+    private String address;
+    private int id_DB=-1;
 
     public String getName() {
         return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
     }
 
     public String getMobile() {
         return mobile;
     }
 
-    public void setMobile(String mobile) {
-        this.mobile = mobile;
-    }
-
     public String getDanwei() {
         return danwei;
-    }
-
-    public void setDanwei(String danwei) {
-        this.danwei = danwei;
     }
 
     public String getQq() {
         return qq;
     }
 
-    public void setQq(String qq) {
-        this.qq = qq;
-    }
-
     public String getAddress() {
         return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
     }
 
     public int getId_DB() {
         return id_DB;
     }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+
+    public void setDanwei(String danwei) {
+        this.danwei = danwei;
+    }
+
+    public void setQq(String qq) {
+        this.qq = qq;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
     public void setId_DB(int id_DB) {
         this.id_DB = id_DB;
     }
-
-
-
-
 }
